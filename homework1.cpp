006b648278002424120ac6f02807e186caf9130c
runes <runes@DESKTOP-064EM22>
2024-11-05 22:04:41 +0300

    Добавьте файлы проекта.

---
+++ homework1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    float x, y;
+
+    std::cout << "Enter x and y values: ";
+    std::cin >> x >> y;
+
+    double R = sin(x / y);
+    double S = exp(-3 * x) + cbrt(y);
+
+    double C = std::max(R, S);
+
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+    std::cout << "C = " << C << std::endl;
+
+    system("pause");
+
+    return 0;
+}
